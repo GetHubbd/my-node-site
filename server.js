6f01a4b293dec930ef1aef18cbd55fddd91fe121
GetHubbd <pettersonthom@gmail.com>
2026-03-11 15:54:13 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+ 
+const app = express()
+ 
+const PORT = 3000
+ 
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    res.send("<h1>This is not alex's website</h1>")
+})
+
+app.get('/', (req, res) => {
+    res.send("<h1>Alex is forbidden fron using this website</h1>")
+})
+
+app.get('/', (req, res) => {
+    res.send("<h1>If Alex tries to enter my website i will tell mr caufield to have him thrown from the classroom</h1>")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`)
+})
